8d4cc85cbd803cffeb94d05ddf1c6610a2954856
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-05 21:30:04 +0530

    Noisy searchbar styles - to be cleaned

--- yt-clone/src/components/AppCard/SearchBar.tsx
+++ yt-clone/src/components/AppCard/SearchBar.tsx
@@ -1,7 +1,13 @@
 export default function SearchBar(){
     return (
         <div>
-            Search
+            <form className="max-w-md mx-auto">   
+                <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div className="relative">
+                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-500 rounded- bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"><svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" /></button>
+                </div>
+            </form>
         </div>
     )
 }
